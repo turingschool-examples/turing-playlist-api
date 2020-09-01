4f9db117b70a8fe92101b448ba03ab0a443df472
Khalid Williams <khalidwill27@gmail.com>
2020-09-01 10:46:51 -0600

    Add a post endpoint for new songs

--- server.js
+++ server.js
@@ -11,22 +11,43 @@ app.locals.playlist = [
   {
     songName: 'Swear',
     artistName: 'Casiopea',
-    link: 'https://www.youtube.com/watch?v=6GEI3PpXEAo&t=1771s'
+    link: 'https://www.youtube.com/watch?v=6GEI3PpXEAo&t=1771s',
+    id: 1
   },
   {
     songName: 'Autumn Leaves',
     artistName: 'Bill Evans Trio',
-    link: 'https://www.youtube.com/watch?v=r-Z8KuwI7Gc'
+    link: 'https://www.youtube.com/watch?v=r-Z8KuwI7Gc',
+    id: 2
   },
   {
-    songName: 'In Your Eyes',
-    artistName: 'BADBADNOTGOOD',
-    link: 'https://www.youtube.com/watch?v=-PCauQozne0'
+    songName: 'Fair Weather',
+    artistName: 'Art Farmer',
+    link: 'https://www.youtube.com/watch?v=8-jfsUusSDQ',
+    id: 3
+  },
+  {
+    songName: 'Django',
+    artistName: 'The Modern JAzz Quartet',
+    link: 'https://www.youtube.com/watch?v=wXnkD7_5vqM',
+    id: 4
   },
 ]
 
 app.get(`${baseUrl}/playlist`, (req, res) => {
   res.status(200).json(app.locals.playlist);
+});
+
+app.post(`${baseUrl}/playlist`, (req, res) => {
+  const { songName, artistName, link } = req.body;
+  for (let prop of [songName, artistName, link]) {
+    if (!prop) {
+      return res.status(422).json({errorMesage: `Cannot POST: missing property ${prop} on request`});
+    }
+  }
+  const newSong = { songName, artistName, link, id: Date.now() };
+  app.locals.playlist.push(newSong);
+  res.status(201).json(newSong);
 });
 
 app.listen(port, () => {
