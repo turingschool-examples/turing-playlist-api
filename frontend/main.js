e398262fb397843fabfb191cfc5875f40345fb50
Kayla Wood <kaylaewood@gmail.com>
2023-11-01 17:21:48 -0600

    Add frontend for activity

---
+++ frontend/main.js
@@ -0,0 +1,55 @@
+// Query Selectors 
+
+const titleInput = document.querySelector('#title-input')
+const artistInput = document.querySelector('#artist-input')
+const addSongButton = document.querySelector('#add-button')
+const playlistContainer = document.querySelector('#playlist')
+
+// Functions
+
+function displaySongs(songs) {
+  songs.forEach(song => {
+   addSong(song);
+  })
+}
+
+function addSong(song) {
+  playlistContainer.innerHTML += `
+    <section class="card">
+      <h2>${song.songName}</h2>
+      <p>${song.artistName}<p>
+    </section>
+    `
+}
+
+// Challenges - Network Requests
+
+// 1. Finish fetchSongs to get all songs from the endpoint and invoke function displaySongs(songs) {
+
+const fetchSongs = () => {
+  // fetch();
+}
+
+fetchSongs();
+
+
+
+// 2. Finish createSong to add a new song to the dataset when a user fills out the form and addSong is invoked
+
+const createSong = (event) => {
+  event.preventDefault();
+
+  // fetch();
+}
+
+addSongButton.addEventListener('click', function(event) {
+  createSong(event);
+});
+
+
+
+// Extensions 
+
+// 3. Add a delete button to each card and use the endpoint to remove the song from the playlist
+
+// 4. Add error handling so that if the form is incomplete, the POST is not sent and the user gets a helpful error message
--- server.js
+++ server.js
@@ -11,25 +11,21 @@ app.locals.playlist = [
   {
     songName: 'Swear',
     artistName: 'Casiopea',
-    link: 'https://www.youtube.com/watch?v=6GEI3PpXEAo&t=1771s',
     id: 1
   },
   {
     songName: 'Autumn Leaves',
     artistName: 'Bill Evans Trio',
-    link: 'https://www.youtube.com/watch?v=r-Z8KuwI7Gc',
     id: 2
   },
   {
     songName: 'Fair Weather',
     artistName: 'Art Farmer',
-    link: 'https://www.youtube.com/watch?v=8-jfsUusSDQ',
     id: 3
   },
   {
     songName: 'Django',
     artistName: 'The Modern Jazz Quartet',
-    link: 'https://www.youtube.com/watch?v=wXnkD7_5vqM',
     id: 4
   },
 ]
@@ -39,13 +35,13 @@ app.get(`${baseUrl}/playlist`, (req, res) => {
 });
 
 app.post(`${baseUrl}/playlist`, (req, res) => {
-  const { songName, artistName, link } = req.body;
-  for (let prop of [songName, artistName, link]) {
-    if (!prop) {
-      return res.status(422).json({errorMesage: `Cannot POST: missing property ${prop} on request`});
-    }
-  }
-  const newSong = { songName, artistName, link, id: Date.now() };
+  const { songName, artistName } = req.body;
+  // for (let prop of [songName, artistName]) {
+  //   if (!prop) {
+  //     return res.status(422).json({errorMesage: `Cannot POST: missing property ${prop} on request`});
+  //   }
+  // }
+  const newSong = { songName, artistName, id: Date.now() };
   app.locals.playlist.push(newSong);
   res.status(201).json(newSong);
 });
